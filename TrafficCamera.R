c2afa78dd9b856f8b08af973550755e7483c6599
Matthew Triner <matt@hunterstrategy.net>
2016-04-03 00:36:27 -0400

    updated R script to loop through all VZ datafiles, identify camera tickets, and compile GeoJson of all street segs that have had traffic tickets

--- TrafficCamera.R
+++ TrafficCamera.R
@@ -3,35 +3,37 @@ library(jsonlite)
 library(rgdal)
 
 
-setwd("C:/Users/admin/WebstormProjects/VisionZeroCameraTickets/data")
-
-#streetSegis <- readOGR("C:/Users/admin/WebstormProjects/VisionZeroCameraTickets/street_segments.geojson", "OGRGeoJSON")
-
-
+setwd("C:/Users/admin/WebstormProjects/DCVisionZeroTicketCamera/data")
 
 ldf <- list() # creates a list
-listcsv <- dir(pattern = "*.csv") # creates the list of all the csv files in the directory
-for (k in 1:length(listcsv)){
-	print(listcsv[k])
+listcsv = list.files(pattern="*.csv")
 
+for (k in 1:length(listcsv)){
 	dataset <- read.csv(file=listcsv[k],header=TRUE,sep=",") 
 	dataset<-subset(dataset, TICKETTYPE=="Photo")
 	segCounts = count(dataset$STREETSEGID)
+
 
 	fileDateString <-  substring(listcsv[k],22,nchar(listcsv[k])-4)
 	fileDateMonth <- substring(fileDateString,0,nchar(fileDateString)-5)
 	fileDateNew <- paste( substring(fileDateString,nchar(fileDateMonth)+2),"_",match(fileDateMonth,month.name), sep = "")
 
-	colnames(segCounts)<-c("STREETSEGID",fileDateNew);
+	colnames(segCounts)<-c("streetsegi",fileDateString);
 
-	print(segCounts)
-
-	 ldf[[k]] <-segCounts
-
+	ldf[[k]] <- segCounts
 }
 
-print(toJSON(ldf))
 
+segBase <- list()
+for (j in 1:length(ldf)){
+segBase <- merge(segBase ,ldf[j], all = TRUE)
+}
+
+
+streetSegis <- readOGR("C:/Users/admin/WebstormProjects/DCVisionZeroTicketCamera/street_segments.geojson", "OGRGeoJSON")
+
+
+streetSegis <- merge(streetSegis,segBase)
 
 #merge impaired driving incidents with the GeoJSON on a per neigbhorhood basis 
-#writeOGR(streetSegis ,"annotatedData.geojson","Streets",driver="GeoJSON");
+writeOGR(streetSegis ,"allTicketsBySeg.geojson","Streets",driver="GeoJSON");
